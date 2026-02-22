ed660c138eddfe077a09e3dcebb1711b24984216
Travis Ross <travis1690@hotmail.co.uk>
2026-02-22 17:56:51 +0000

    refactor: :art: extract list of habits into HabitsList component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,11 @@
 import EmptyMessage from "@/components/features/EmptyMessage";
 import { auth } from "../_lib/auth";
 import { getHabitsByUserId } from "@/lib/data-service";
+import HabitsList from "@/components/features/Habits";
+
+export const metadata = {
+  title: "Dashboard",
+};
 
 export default async function Dashboard() {
   const session = await auth();
@@ -15,27 +20,7 @@ export default async function Dashboard() {
       <p className="flex justify-center text-2xl font-bold mb-4">
         Hello, {session?.user?.name ?? "Guest"}!
       </p>
-      {habits.length > 0 ? (
-        <ul className="space-y-4">
-          {habits.map((habit) => (
-            <li
-              key={habit.id}
-              className="p-4 bg-white rounded shadow flex items-center space-x-4"
-            >
-              <div
-                className="w-4 h-4 rounded-full"
-                style={{ backgroundColor: habit.color ?? "#ccc" }}
-              />
-              <div>
-                <p className="font-semibold">{habit.name}</p>
-                <p className="text-sm text-gray-500">{habit.frequency}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <EmptyMessage />
-      )}
+      {habits.length > 0 ? <HabitsList habits={habits} /> : <EmptyMessage />}
     </>
   );
 }
