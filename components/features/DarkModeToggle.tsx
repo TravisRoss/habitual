b014ad554f11e9a9d0b89407253f3135547ea89c
Travis Ross <travis1690@hotmail.co.uk>
2026-02-26 18:43:31 +0000

    fix: fix Hydration mismatch between server and client

--- components/features/DarkModeToggle.tsx
+++ components/features/DarkModeToggle.tsx
@@ -12,7 +12,11 @@ export function DarkModeToggle() {
       size="icon"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      {theme === "dark" ? <Sun className="h-5 w-5" /> : <Moon className="h-5 w-5" />}
+      {theme === "dark" || theme === undefined ? (
+        <Sun className="h-5 w-5" />
+      ) : (
+        <Moon className="h-5 w-5" />
+      )}
     </Button>
   );
 }
