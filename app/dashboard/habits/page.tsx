c5a22ff0a0c3f34fb74d6a10650fdabd466aabba
Travis Ross <travis1690@hotmail.co.uk>
2026-03-02 09:27:09 +0000

    feat: :sparkles: add a back button to the habits page to go back to dashboard

--- app/dashboard/habits/page.tsx
+++ app/dashboard/habits/page.tsx
@@ -4,6 +4,9 @@ import HabitsList from "@/components/features/HabitsList";
 import { formatDate } from "../../_lib/utils";
 import { DateStrip } from "@/components/features/DateStrip";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { MoveLeft } from "lucide-react";
 
 export default function Page() {
   const [selectedDate, setSelectedDate] = useState(formatDate());
@@ -11,6 +14,17 @@ export default function Page() {
   return (
     <>
       <div className="flex flex-col gap-2">
+        <Button
+          asChild
+          variant="link"
+          size="sm"
+          className="w-max hover:text-brand"
+        >
+          <Link href="/dashboard">
+            <MoveLeft className="w-2 h-2 mr-1" />
+            Back to Dashboard
+          </Link>
+        </Button>
         <DateStrip selectedDate={selectedDate} onSelect={setSelectedDate} />
         <HabitsList date={selectedDate} />
       </div>
