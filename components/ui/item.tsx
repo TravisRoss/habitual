d802df9d69d443d147ead0edb565a84b3acfe531
Travis Ross <travis1690@hotmail.co.uk>
2026-03-15 15:16:56 +0000

    fix: :bug: let browser handle pinch zoom natively (utility touch-pinch-zoom on Item component)

--- components/ui/item.tsx
+++ components/ui/item.tsx
@@ -31,7 +31,7 @@ function ItemSeparator({
 }
 
 const itemVariants = cva(
-  "group/item flex items-center border border-transparent text-sm rounded-md transition-colors [a]:hover:bg-accent/50 [a]:transition-colors duration-100 flex-wrap outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] touch-pan-y",
+  "group/item flex items-center border border-transparent text-sm rounded-md transition-colors [a]:hover:bg-accent/50 [a]:transition-colors duration-100 flex-wrap outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] touch-pan-y touch-pinch-zoom",
   {
     variants: {
       variant: {
